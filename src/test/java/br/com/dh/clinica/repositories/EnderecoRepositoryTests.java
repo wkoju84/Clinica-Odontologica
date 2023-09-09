c0e0c100f84434c79ea8e2638bfada1dc2f80332
wkoju84 <wkoju84@gmail.com>
2023-09-09 12:10:58 -0300

    adicionando testes unitários - findAll e save em Endereco

--- src/test/java/br/com/dh/clinica/repositories/EnderecoRepositoryTests.java
+++ src/test/java/br/com/dh/clinica/repositories/EnderecoRepositoryTests.java
@@ -1,12 +1,16 @@
 package br.com.dh.clinica.repositories;
 
+import br.com.dh.clinica.entities.Dentista;
 import br.com.dh.clinica.entities.Endereco;
+import br.com.dh.clinica.tests.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @DataJpaTest
@@ -18,11 +22,33 @@ public class EnderecoRepositoryTests {
     private Integer idExistente;
     private Integer idInexistente;
 
+    private Integer countTotalEndereco;
+
+    private List<Endereco> enderecoList;
+
     @BeforeEach
     void setup() throws Exception{
 
         idExistente = 1;
         idInexistente = 99;
+        countTotalEndereco = 3;
+        enderecoList = new ArrayList<>();
+
+    }
+
+    @Test
+    public void saveDeveriaSalvarComAutoincrementoQuandoOIdForNulo(){
+        Endereco endereco = Factory.criarEndereco();//Simula um DTO
+        endereco = enderecoRepository.save(endereco);
+
+        Assertions.assertNotNull(endereco.getId());
+        Assertions.assertEquals(countTotalEndereco + 1, endereco.getId());
+    }
+
+    @Test
+    public void findAllDeveriaRetornarUmaLista(){
+        enderecoList = enderecoRepository.findAll();
+        Assertions.assertNotNull(enderecoList);
     }
 
     @Test
--- src/test/java/br/com/dh/clinica/tests/Factory.java
+++ src/test/java/br/com/dh/clinica/tests/Factory.java
@@ -1,7 +1,9 @@
 package br.com.dh.clinica.tests;
 
 import br.com.dh.clinica.dtos.DentistaDto;
+import br.com.dh.clinica.dtos.EnderecoDto;
 import br.com.dh.clinica.entities.Dentista;
+import br.com.dh.clinica.entities.Endereco;
 
 public class Factory {
 
@@ -14,5 +16,15 @@ public class Factory {
     public static DentistaDto criarDentistaDto(){
         Dentista dentista = criarDentista();
         return new DentistaDto(dentista);
+    }
+
+    public static Endereco criarEndereco(){
+        Endereco endereco = new Endereco(null, "Rua das Camélias", "110", "Bairro Novo", "SP", "07781-471", "SP");
+        return endereco;
+    }
+
+    public static EnderecoDto criarEnderecoDto(){
+        Endereco endereco = criarEndereco();
+        return new EnderecoDto(endereco);
     }
 }
