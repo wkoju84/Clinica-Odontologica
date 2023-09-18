b4f1fa080bafefa95a5133f5674d9c23609d4eeb
wkoju84 <wkoju84@gmail.com>
2023-09-18 10:12:17 -0300

    adicionando WebSecurityConfig e Overriders

--- src/main/java/br/com/dh/clinica/config/WebSecurityConfig.java
+++ src/main/java/br/com/dh/clinica/config/WebSecurityConfig.java
@@ -1,7 +1,11 @@
 package br.com.dh.clinica.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,5 +21,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Override //Configurar a criptografia e o UserDetailService
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
 
+    @Override // Liberar o uso do Actuator para o Oauth2
+    public void configure(WebSecurity webSecurity) throws Exception{
+        webSecurity.ignoring().antMatchers("/actuator/**");
+    }
+
+    @Override // config do spring security
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception{
+        return super.authenticationManager();
+    }
 }
